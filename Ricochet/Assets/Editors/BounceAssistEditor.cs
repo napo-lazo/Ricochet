cdbab2e3ae227e33a427f109390f2b883908e176
Jose Napoleon Lazo Celaya <napoleon.lazo@hotmail.com>
2023-04-01 19:40:00 -0600

    Added a bounce assist to the disc movement
    Added a bounce assist to make it easier for the player to hit enemies when ricocheting the disc off a wall or other enemies

---
+++ Ricochet/Assets/Editors/BounceAssistEditor.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(DiscMovement))]
+public class BounceAssistEditor : Editor
+{
+    private Vector3 DirectionFromAngle(float angleInDegrees)
+    {
+        return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), 0, Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
+    }
+
+    private void OnSceneGUI()
+    {
+        DiscMovement disc = (DiscMovement)target;
+        float viewAngle = disc.viewAngle;
+        float viewRadius = disc.viewRadius;
+        Handles.color = Color.black;
+        Vector3 viewAngleA = DirectionFromAngle(-viewAngle / 2 + disc.transform.eulerAngles.y);
+        Vector3 viewAngleB = DirectionFromAngle(viewAngle / 2 + disc.transform.eulerAngles.y);
+
+        Handles.DrawWireArc(disc.transform.position, Vector3.up, Vector3.forward, 360, viewRadius);
+        Handles.DrawLine(disc.transform.position, disc.transform.position + viewAngleA * viewRadius);
+        Handles.DrawLine(disc.transform.position, disc.transform.position + viewAngleB * viewRadius);
+    }
+}
--- Ricochet/Assets/Scripts/DiscMovement.cs
+++ Ricochet/Assets/Scripts/DiscMovement.cs
@@ -9,10 +9,27 @@ public class DiscMovement : MonoBehaviour
     private float timePassed = 0f;
     private bool recall = false;
     private Transform player;
+    private List<int> idsOfLastEnemiesHit = new List<int>();
 
+    [Header("Movement settings")]
     public float discSpeed = 10f;
     public float secondsFlying = 1f;
+
+    [Header("Bounce settings")]
+    [Tooltip("How many previously hit enemies to keep track of to prevent the disc from getting stuck on a bounce loop")]
+    public int EnemyBounceLoopPrevention = 2;
+
+    [Header("Field of view")]
+    public float viewRadius = 25f;
+    [Range(0, 360)]
+    public float viewAngle = 70f;
+    
+    [Header("Debug settings")]
     public bool debugCollisions = false;
+
+    [Header("Layers")]
+    public LayerMask enemyMask;
+    public LayerMask obstacleMask;
 
     public void setPlayer(Transform player)
     {
@@ -51,17 +68,55 @@ public class DiscMovement : MonoBehaviour
         }
         else
         {
-            Vector3 moveTo = player.position - transform.position;
-            moveTo = moveTo.normalized * discSpeed;
-            rb.velocity = Vector3.zero;
-            transform.rotation = Quaternion.LookRotation(moveTo.normalized, transform.up);
-            rb.AddForce(moveTo, ForceMode.Force);
+            if (player != null)
+            {
+                Vector3 moveTo = player.position - transform.position;
+                moveTo = moveTo.normalized * discSpeed;
+                rb.velocity = Vector3.zero;
+                transform.rotation = Quaternion.LookRotation(moveTo.normalized, transform.up);
+                rb.AddForce(moveTo, ForceMode.Force);
+            }
         }
+    }
+
+    private Collider GetNearestEnemy()
+    {
+        float nearestDistance = 0;
+        Collider nearestEnemy = null;
+        Collider[] targetsInViewRadius = Physics.OverlapSphere(transform.position, viewRadius, enemyMask);
+
+        foreach (Collider collider in targetsInViewRadius)
+        {
+            Transform target = collider.transform;
+            Vector3 directionToTarget = (target.position - transform.position).normalized;
+            if (Vector3.Angle(transform.forward, directionToTarget) < viewAngle / 2)
+            {
+                int instanceId = collider.GetInstanceID();
+                float distanceToTarget = Vector3.Distance(transform.position, target.position);
+                if (!idsOfLastEnemiesHit.Contains(instanceId) && !Physics.Raycast(transform.position, directionToTarget, distanceToTarget, obstacleMask))
+                {
+                    if (distanceToTarget < nearestDistance || nearestDistance == 0)
+                    {
+                        nearestDistance = distanceToTarget;
+                        nearestEnemy = collider;
+                    }
+                }
+            }
+        }
+
+        return nearestEnemy;
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (recall)
+        if (enemyMask ==  (enemyMask | (1 << collision.gameObject.layer)))
+        {
+            idsOfLastEnemiesHit.Add(collision.collider.GetInstanceID());
+            if (idsOfLastEnemiesHit.Count > EnemyBounceLoopPrevention)
+                idsOfLastEnemiesHit.RemoveAt(0);
+        }
+
+        if (player != null && recall)
         {
             recall = false;
             rb.useGravity = true;
@@ -78,13 +133,29 @@ public class DiscMovement : MonoBehaviour
 
                 if (debugCollisions)
                 {
-                    Debug.DrawLine(contact.point, contact.point + contact.normal, Color.red, 5f, false);
-                    Debug.DrawLine(contact.point, contact.point + collision.relativeVelocity, Color.blue, 5f, false);
-                    Debug.DrawLine(contact.point, contact.point + Vector3.Reflect(-collision.relativeVelocity, contact.normal), Color.green, 5f, false);
+                    Debug.DrawLine(contact.point, contact.point + contact.normal, Color.red);
+                    Debug.DrawLine(contact.point, contact.point + collision.relativeVelocity, Color.blue);
+                    Debug.DrawLine(contact.point, contact.point + Vector3.Reflect(-collision.relativeVelocity, contact.normal), Color.green);
+                    Debug.Break();
                 }
 
                 rb.velocity = Vector3.zero;
                 transform.rotation = Quaternion.LookRotation(bounce.normalized, transform.up);
+
+                //bounce assist
+                Collider nearestEnemy = GetNearestEnemy();
+                if (nearestEnemy != null)
+                {
+                    if (debugCollisions)
+                    {
+                        Debug.DrawRay(transform.position, nearestEnemy.transform.position - transform.position, Color.magenta);
+                        Debug.Break();
+                    }
+
+                    bounce = (nearestEnemy.transform.position - transform.position).normalized * discSpeed;
+                    transform.rotation = Quaternion.LookRotation(bounce.normalized, transform.up);
+                }
+
                 rb.AddForce(bounce, ForceMode.Force);
             }
         }
